5ad1b4e9d90bc6b4b2705de0167f4279cf693694
ComPups <amartnez@uc.cl>
2024-04-18 15:50:21 -0400

    feat(App.jsx):change in auth managment

--- src/common/App.jsx
+++ src/common/App.jsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import reactLogo from './../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LoginButton from '../profile/LoginButton'
+import LogoutButton from '../profile/LogoutButton'
+import { useAuth0 } from '@auth0/auth0-react'
+
+
 function App() {
+
+  const {isAuthenticated} = useAuth0()
+
 
   return (
     <div className="App">
-      <a href='/signup'>Ir a Signup</a>
-      <a href='/login'>Ir a Login</a>
-      <a href='/listingflights'>Ir a Listingflights</a>
+      {isAuthenticated && (
+      <div>
+      <LogoutButton />
+      <a href='/welcome'> Welcome</a>
+      <a href='/listingflights'> Flights</a>
+        </div>
+      
+      )}
+      {!isAuthenticated && (
+        <LoginButton/>
+      )}
+      
     </div>
   )
 }
--- src/common/main.jsx
+++ src/common/main.jsx
@@ -4,9 +4,20 @@ import App from './App.jsx'
 import './index.css'
 import UserWelcome from '../profile/UserWelcome.jsx'
 import Routing from './Routing.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider
+      domain="dev-8v3xbi0ihbyftsnr.us.auth0.com"
+      clientId="5bLCwk5Hdl24LBMAhhdv3dYQJuEb5ZIA"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
     <Routing />
+    </Auth0Provider>
   </React.StrictMode>
 )
