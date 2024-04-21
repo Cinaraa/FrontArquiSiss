e2c8581b7ee77642406c181ce1fff0e30fc87e05
ComPups <amartnez@uc.cl>
2024-04-21 13:19:28 -0400

    feat: getting address

--- src/profile/Profileinfo.jsx
+++ src/profile/Profileinfo.jsx
@@ -1,27 +1,49 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
+const Profileinfo = () => {
+    const { user, isAuthenticated } = useAuth0();
+    const [ip, setIP] = useState("");
+    const [location, setLocation] = useState(null);
+    const [address, setAddress] = useState("");
 
-const Profileinfo = ()=> {
-    const {user, isLoading , isAuthenticated} = useAuth0();
-    console.log(isAuthenticated);
-    if (!isLoading) {
-        const userId = user.sub;
-        console.log('userId:', userId);
-    }
-    
-    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Obtener la dirección IP
+                const ipRes = await axios.get("https://api.ipify.org/?format=json");
+                const userIP = ipRes.data.ip;
+                setIP(userIP);
+
+                // Obtener la información de ubicación
+                const locationRes = await axios.get(`https://api.geoapify.com/v1/ipinfo?apiKey=c4483a9a22c74a52850e281789f5e38c&ip=${userIP}`);
+                setLocation(locationRes.data);
+
+                // Obtener la dirección a partir de la latitud y longitud
+                const { latitude, longitude } = locationRes.data.location;
+                const addressRes = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=c4483a9a22c74a52850e281789f5e38c`);
+                setAddress(addressRes.data.features[0].properties.formatted);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        if (isAuthenticated) {
+            fetchData();
+        }
+    }, [isAuthenticated]);
+
     return (
-        
         isAuthenticated && (
             <div>
                 <h1>Bienvenido, {user.name}</h1>
                 <img src={user.picture} alt={user.name} />
+                <h2>Tu IP es: {ip}</h2>
+                {address && <h2>Dirección: {address}</h2>}
             </div>
         )
-            );
-        };
+    );
+};
 
 export default Profileinfo;
-        
-        
