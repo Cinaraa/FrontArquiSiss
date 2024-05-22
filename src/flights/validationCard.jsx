a7f7ff7ebd1ee8c85540a0dda30231d050353c98
LukasGuthrie <lguthrie@uc.cl>
2024-05-22 05:20:59 -0400

    instrucciones de vista de redireccion desde flujo webpay

---
+++ src/flights/validationCard.jsx
@@ -0,0 +1,5 @@
+/*
+Aqui va el UI que continua cuadose termina el flujo Webpay, se redirecciona
+ Y se pega al endpoint para verificar si la transaccion fue interrupida,
+exitosa o fallida (post en /flights/transactions/:request_id)
+*/
